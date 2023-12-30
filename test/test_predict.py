2f834a646e14bd1d67cc7b78ec21e53f29b72c35
Amaldev C <amaldev.c@miqdigital.com>
2023-12-30 16:16:23 +0530

    Format test files

--- test/test_predict.py
+++ test/test_predict.py
@@ -2,13 +2,16 @@ from pandas import DataFrame
 from fire_ext_model.processing.predict import Predict
 from fire_ext_model.processing.train import Train
 
-class TestPredict:
 
-    def test_prediction(self,trained_model:Train):
-        data = [[2,b'gasoline',30,101,0.4,70]]
-        df = DataFrame(data,columns=['SIZE','FUEL','DISTANCE','DESIBEL','AIRFLOW','FREQUENCY'])
+class TestPredict:
+    def test_prediction(self, trained_model: Train):
+        data = [[1, b"gasoline", 30, 101, 0.4, 70]]
+        df = DataFrame(
+            data,
+            columns=["SIZE", "FUEL", "DISTANCE", "DESIBEL", "AIRFLOW", "FREQUENCY"],
+        )
         pred_obj = Predict(trained_model)
 
         pred_array = pred_obj.predict(df)
-        result = [1 if x >= 0.5 else 0 for x in pred_array[:,0]]
+        result = [1 if x >= 0.5 else 0 for x in pred_array[:, 0]]
         assert result[0] == 1
